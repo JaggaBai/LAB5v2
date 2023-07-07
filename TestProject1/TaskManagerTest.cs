e38993e3b2b56ea20d148658b25750945c717374
Jagga Bai <dzagur@post.pl>
2023-07-07 21:16:20 +0200

    Testy- 2metody testowe done

--- TestProject1/TaskManagerTest.cs
+++ TestProject1/TaskManagerTest.cs
@@ -25,5 +25,29 @@ namespace TestProject1
             // Assert
             Assert.AreEqual(1, _taskManager.GetTasks().Count);
         }
+        [Test]
+        public void RemoveTask_ExistingTask_ShouldDecreaseTaskCount()
+        {
+            // Arrange
+            var task1 = new LAB5.Model.Task("Test task");
+            _taskManager.AddTask(task1);
+            // Act
+            _taskManager.RemoveTask(task1.Id);
+            // Assert
+            Assert.AreEqual(0, _taskManager.GetTasks().Count);
+        }
+        [Test]
+        public void GetTasks_ShouldReturnAllTasks()
+        {
+            // Arrange
+            var task3 = new LAB5.Model.Task("Test task");
+            var task4 = new LAB5.Model.Task("Test task");
+            _taskManager.AddTask(task3);
+            _taskManager.AddTask(task4);
+            // Act
+            _taskManager.GetTasks();
+            // Assert
+            Assert.AreEqual(2, _taskManager.GetTasks().Count);
+        }
     }
 }
